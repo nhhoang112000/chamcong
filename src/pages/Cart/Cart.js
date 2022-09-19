452ede49aa7079d58d2d878cc4b537f324f33089
Hoang <nguyenhuyhoang112k@gmail.com>
2022-09-19 23:12:54 +0700

    fix name table column

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { DataGrid, GridRowsProp, GridColDef, GridActionsCellItem } from '@mui/x-data-grid';
 import { useState, useCallback } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import classNames from 'classnames/bind';
 import styles from './Cart.module.scss';
 
@@ -10,43 +11,56 @@ function Cart() {
     const [pageSize, setPageSize] = useState(5);
 
     const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        { id: 1, color: 'black', modelName: 'Jon', price: 35 },
+        { id: 2, color: 'red', modelName: 'Cersei', price: 42 },
+        { id: 3, color: 'yellow', modelName: 'Jaime', price: 45 },
+        { id: 4, color: 'white', modelName: 'Arya', price: 16 },
+        { id: 5, color: 'green', modelName: 'Daenerys', price: null },
+        { id: 6, color: 'blue', modelName: null, price: 150 },
+        { id: 7, color: 'orange', modelName: 'Ferrara', price: 44 },
+        { id: 8, color: 'grey', modelName: 'Rossini', price: 36 },
+        { id: 9, color: 'silver', modelName: 'Harvey', price: 65 },
     ];
     const columns = [
         {
             field: 'id',
             headerName: 'ID',
-            width: 70,
+            width: 120,
         },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+        { field: 'modelName', headerName: 'Model name', width: 250 },
+        { field: 'color', headerName: 'Color', width: 250 },
         {
-            field: 'age',
-            headerName: 'Age',
+            field: 'price',
+            headerName: 'Price',
             type: 'number',
-            width: 90,
+            width: 250,
         },
         {
-            field: 'fullName',
-            headerName: 'Full name',
+            field: 'address',
+            headerName: 'Address',
             description: 'This column has a value getter and is not sortable.',
             sortable: false,
             width: 160,
-            valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+            valueGetter: (params) => `${params.row.modelName || ''} ${params.row.color || ''}`,
         },
         {
-            field: 'actions',
+            field: 'time',
+            headerName: 'Time',
+            type: 'datetime',
+            width: 300,
+            valueGetter: () => `${Date().toLocaleString()}`,
+        },
+        {
+            field: 'edit',
+            type: 'actions',
+            getActions: (params) => [<GridActionsCellItem icon={<EditIcon />} label="Edit" />],
+        },
+        {
+            field: 'delete',
             type: 'actions',
             getActions: (params) => [<GridActionsCellItem icon={<DeleteIcon />} label="Delete" />],
         },
+
     ];
 
     // const deleteUser = useCallback(
